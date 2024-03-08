57e3ab855c2c1768ae197648fdca193901ea43eb
Luc Stepniewski <luc@eclipse-fr.com>
2024-03-08 10:17:42 +0100

    Initial release

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lstep/chrometabcounter
+
+go 1.21.8
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"flag"
+	"strings"
+	"net/http"
+	"os"
+)
+
+var (
+	chromeUrl *string
+)
+
+// Tab represents a Chrome tab.
+type Tab struct {
+	Description          string `json:"description"`
+	DevtoolsFrontendUrl string `json:"devtoolsFrontendUrl"`
+	FaviconUrl           string `json:"faviconUrl"`
+	Id                   string `json:"id"`
+	ThumbnailUrl         string `json:"thumbnailUrl"`
+	Title                string `json:"title"`
+	Type                 string `json:"type"`
+	Url                 string `json:"url"`
+	WebSocketDebuggerUrl string `json:"webSocketDebuggerUrl"`
+}
+
+// GetTabs retrieves a list of open tabs from a Chrome instance with remote debugging enabled.
+func GetTabs() ([]Tab, error) {
+	resp, err := http.Get(*chromeUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var tabs []Tab
+	err = json.NewDecoder(resp.Body).Decode(&tabs)
+	if err != nil {
+		return nil, err
+	}
+
+	return tabs, nil
+}
+
+func CountTabs() int {
+	tabs, err := GetTabs()
+	if err != nil {
+		fmt.Println("Error retrieving tabs:", err)
+		os.Exit(1)
+	}
+
+	count := 0
+	for _, tab := range tabs {
+		if tab.Type == "page" {
+			if strings.Contains(tab.Url, "park.html") || strings.HasPrefix(tab.Url, "http") {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func main() {
+	chromeUrl = flag.String("chromeUrl", "http://localhost:9222/json", "URL for connecting to Chrome")
+	flag.Parse()
+
+	nbTabs := CountTabs()
+	fmt.Printf("Number of open tabs: %d\n", nbTabs)
+}
